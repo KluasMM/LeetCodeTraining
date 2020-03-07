9e1240d70d3b9f52b620624faef0fcdf34fb804d
“KluasMM” <matian0720@foxmail.com>
2020-03-07 19:47:13 +0800

    first commmit

---
+++ src/main/java/com/mt/DoublePointer/Test1.java
@@ -0,0 +1,18 @@
+package com.mt.DoublePointer;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Klaus on 2020/3/7.
+ */
+@RestController
+public class Test1 {
+
+    @RequestMapping("/test1")
+    public String test1(){
+        String a = "123";
+        System.out.println(a);
+        return a;
+    }
+}
