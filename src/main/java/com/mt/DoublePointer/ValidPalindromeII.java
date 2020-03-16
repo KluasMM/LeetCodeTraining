01c75c8ee7a311413f21f5227ef050183e8e538c
KluasMM <matian0720@foxmail.com>
2020-03-16 14:45:47 +0800

    刷题

---
+++ src/main/java/com/mt/DoublePointer/ValidPalindromeII.java
@@ -0,0 +1,29 @@
+package com.mt.DoublePointer;
+
+public class ValidPalindromeII {
+    /**
+     * Given a non-empty string s, you may delete at most one character. Judge whether you can make it a palindrome.
+     */
+    public static void main(String[] args) {
+        boolean result = validPalindrome("aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga");
+        System.out.println(result);
+    }
+    public static boolean validPalindrome(String s) {
+        for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return isPalindrome(s, i, j - 1) || isPalindrome(s, i + 1, j);
+            }
+        }
+        return true;
+    }
+
+    private static boolean isPalindrome(String s, int i, int j) {
+        while (i < j) {
+            if (s.charAt(i++) != s.charAt(j--)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
